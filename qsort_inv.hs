6c5184d2fc018f2f878caf2c6f07afdf889b5a0d
Juliano Silva <juliano.silva2011@gmail.com>
2019-10-15 20:22:24 -0300

    fix name of quick sort inverse func

--- qsort_inv.hs
+++ qsort_inv.hs
@@ -1,6 +1,6 @@
-qsort :: Ord a => [a] -> [a]
-qsort [] = []
-qsort (x:xs) = qsort larger ++ [x] ++ qsort smaller
+qsort_inv :: Ord a => [a] -> [a]
+qsort_inv [] = []
+qsort_inv (x:xs) = qsort_inv larger ++ [x] ++ qsort_inv smaller
 	where
 		smaller = [a | a <- xs, a <= x]
 		larger = [b | b <- xs, b > x]
